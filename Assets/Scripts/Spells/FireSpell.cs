53fef03b00e3fb6a0d710330f597828878e2f302
Yalkın Kömüç <yalkinkomuc4@gmail.com>
2025-03-23 12:40:59 +0300

    firespell eklendi
    buglar var hala

--- Assets/Scripts/Spells/FireSpell.cs
+++ Assets/Scripts/Spells/FireSpell.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class FireSpell : MonoBehaviour
 {
-    [SerializeField] private int damage = 15;
+    [SerializeField] private int damagePerTick = 5; // Her tick'te verilecek hasar
+    [SerializeField] private float tickRate = 0.2f; // Kaç saniyede bir hasar verilecek
     private BoxCollider2D boxCollider2D;
+    private List<Enemy> burningEnemies = new List<Enemy>();
 
     private void Awake()
     {
@@ -12,6 +16,17 @@ public class FireSpell : MonoBehaviour
         {
             boxCollider2D.enabled = false;
         }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DamageOverTime());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        burningEnemies.Clear();
     }
 
     public void EnableDamage()
@@ -33,9 +48,40 @@ public class FireSpell : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Enemy enemy = other.GetComponent<Enemy>();
-        if (enemy != null)
+        if (enemy != null && !burningEnemies.Contains(enemy))
         {
-            enemy.stats.TakeDamage(damage);
+            burningEnemies.Add(enemy);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if ( enemy!= null)
+        {
+            burningEnemies.Remove(enemy);
+        }
+    }
+
+    private IEnumerator DamageOverTime()
+    {
+        while (true)
+        {
+            // Listede olan tüm düşmanlara periyodik hasar ver
+            for (int i = burningEnemies.Count - 1; i >= 0; i--)
+            {
+                if (burningEnemies[i] != null)
+                {
+                    burningEnemies[i].DamageWithoutKnockback();
+                    burningEnemies[i].stats.TakeDamage(damagePerTick);
+                }
+                else
+                {
+                    burningEnemies.RemoveAt(i);
+                }
+            }
+
+            yield return new WaitForSeconds(tickRate);
         }
     }
 } 
