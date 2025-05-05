7f421b73a9ac76cfab6e1932d299ccdb73445a0f
Yalkın Kömüç <yalkinkomuc4@gmail.com>
2025-05-05 14:52:05 +0300

    update

--- Assets/Scripts/Inventory/Items/SkillShard.cs
+++ Assets/Scripts/Inventory/Items/SkillShard.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 public class SkillShard : ItemData
 {
     [Header("Skill Shard Properties")]
-    [SerializeField] private int shardValue = 1; // Bir shard kaç puan değerinde
+    [SerializeField] private int shardValue = 4; // Bir shard kaç puan değerinde
 
     public int GetShardValue()
     {
+        if (itemName == "Small Shard")
+        {
+            shardValue = Random.Range(4, 12);
+        }
+        
+        if (itemName == "Medium Shard")
+        {
+            shardValue = Random.Range(12, 25);
+        }
+        
+        if (itemName == "Large Shard")
+        {
+            shardValue = Random.Range(25, 40);
+        }
+        
+        if (itemName == "XLarge Shard")
+        {
+            shardValue = Random.Range(40, 75);
+        }
+        
         return shardValue;
     }
 } 
